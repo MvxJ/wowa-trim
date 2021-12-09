e14df5a5c6a27b0c6cfeb45309734f15c2c65ea4
MvxJ <58326571+MvxJ@users.noreply.github.com>
2021-12-09 17:49:50 +0100

    Correct email format

--- sendmail.php
+++ sendmail.php
@@ -6,16 +6,55 @@ $senderName = $_POST['name'];
 $senderSurname = $_POST['surname'];
 $senderNumber = $_POST['number'];
 $senderEmail = $_POST['email'];
-$message = 'Imie: ' . $senderName . '\n Nazwisko: ' . $senderSurname . '\n Numer kontaktowy: ' . $senderNumber . '\n Adres email: ' . $senderEmail . '\n Wiadomość: ' . $_POST['message'];
 
-$mailSubject = 'WOWA TRIM - mail submission | ' . $senderName . ' ' . $senderSurname; 
-$mailHeaders = 'From:' . $senderName . ' ' . $senderSurname . '<' . $senderEmail . '>\r\n';
+
+
+$message = '
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+             <title></title>
+        <style>
+            .canvas {
+                width: 100%;
+                height: 100%;
+                text-align: center;
+            }
+
+             #email-wrap {
+                background: #151515;
+                color: #FFF;
+                padding: 15px;
+            }
+
+            #email-wrap h3 {
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+         <div class="canvas">
+            <div id="email-wrap">
+                <h3>Nowa wiadomość z wowa.com.pl</h3>
+                <p>Imię: ' . $senderName  . '</p> 
+                <p>Nazwisko: ' . $senderSurname . ' </p> 
+                <p>Numer kontaktowy: ' . $senderNumber . '</p>
+                <p>Adres email: ' . $senderEmail . '</p>
+                <p>Wiadomość: ' . $_POST['message'] . '</p>
+            </div>
+        </div>
+    </body>
+</html>
+';
+
+$mailSubject = 'WOWA-TRIM - wiadomość z strony www | ' . $senderName . ' ' . $senderSurname; 
+$mailHeaders = 'Content-type:text/html;charset=utf-8 From:' . $senderName . ' ' . $senderSurname . '<' . $senderEmail . '>\r\n';
 $mailTarget = 'biuro@wowa.com.pl';
 
 $mailResponse = mail($mailTarget, $mailSubject, $message, $mailHeaders);
 
 if ($mailResponse) {
-    header("Location: index.html");
+    header("Location: index.html#wowa-team-contact");
     die();
 }
 
