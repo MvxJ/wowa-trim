ccb64e1946d6ddf39a18c0a06c535f0e2a02b3d9
MvxJ <58326571+MvxJ@users.noreply.github.com>
2021-12-15 19:23:10 +0100

    Add controll buttons to gallery

--- assets/scripts/custom-gallery.js
+++ assets/scripts/custom-gallery.js
@@ -1,6 +1,8 @@
 const gardenGalleryBox = document.querySelector('#gardenBox');
 const toiletsGalleryBox = document.querySelector('#toiletsBox');
 const roomsGalleryBox = document.querySelector('#roomsBox');
+const secondToiletsGallery = document.querySelector("#next-page-gallery");
+const firstToiletsGallery = document.querySelector("#prev-page-gallery");
 
 gardenGalleryBox.addEventListener('click', e => {
     const activeGallery = document.querySelectorAll('.activeGallery');
@@ -15,13 +17,29 @@ gardenGalleryBox.addEventListener('click', e => {
 
 toiletsGalleryBox.addEventListener('click', e => {
     const activeGallery = document.querySelectorAll('.activeGallery');
-    const activeGallerySelect =document.querySelectorAll('.activeGalletySelect')
+    const activeGallerySelect = document.querySelectorAll('.activeGalletySelect')
     const toiletsGallery = document.querySelector('#toilets');
 
     activeGallery[0].classList.remove('activeGallery'); 
     activeGallerySelect['0'].classList.remove('activeGalletySelect');
     toiletsGallery.classList.add('activeGallery');
     toiletsGalleryBox.classList.add('activeGalletySelect');
+});
+
+secondToiletsGallery.addEventListener('click', e => {
+    const activeGallery = document.querySelectorAll('.activeGallery');
+    const secondToiletsGallery = document.querySelector('#toilets-second');
+
+    activeGallery[0].classList.remove('activeGallery'); 
+    secondToiletsGallery.classList.add('activeGallery');
+});
+
+firstToiletsGallery.addEventListener('click', e => {
+    const activeGallery = document.querySelectorAll('.activeGallery');
+    const secondToiletsGallery = document.querySelector('#toilets');
+
+    activeGallery[0].classList.remove('activeGallery'); 
+    secondToiletsGallery.classList.add('activeGallery');
 });
 
 roomsGalleryBox.addEventListener('click', e => {
